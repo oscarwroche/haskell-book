190937f9716afc087ba437154ba529feec60bf61
Oscar Roche <oscar.w.roche@gmail.com>
2018-08-22 15:49:17 +0200

    chap 15

---
+++ Chap15.hs
@@ -0,0 +1,76 @@
+module Chap15 where
+
+import Control.Monad
+import Data.Monoid
+import Test.QuickCheck
+
+data Booly a =
+    False'
+  | True'
+  deriving (Eq, Show)
+
+instance Monoid (Booly a) where
+  mappend False' _ = False'
+  mappend _ False' = False'
+  mappend True' True' = True'
+
+data Optional a =
+    Nada
+  | Only a
+  deriving (Eq, Show)
+
+instance Monoid a => Monoid (Optional a) where
+  mempty = Nada
+  mappend (Only x) (Only y) = Only (mappend x y)
+  mappend (Only x) _ = Only x
+  mappend _ (Only y) = Only y
+
+monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
+
+data Bull =
+    Fools
+  | Twoo
+  deriving (Eq, Show)
+
+instance Arbitrary Bull where
+  arbitrary =
+    frequency [ (1, return Fools)
+              , (1, return Twoo) ]
+
+instance Monoid Bull where
+  mempty = Fools
+  mappend _ _ = Fools
+
+type BullMappend = Bull -> Bull -> Bull -> Bool
+
+newtype First' a =
+  First' { getFirst' :: Optional a }
+  deriving (Eq, Show)
+
+instance Monoid (First' a) where
+  mempty = First' Nada
+  mappend (First' x) _ = First' x
+  mappend _ (First' x) = First' x
+  mappend _ _ = First' Nada
+
+firstMappend :: First' a -> First' a -> First' a
+firstMappend = mappend
+
+type FirstMappend =
+     First' String
+  -> First' String
+  -> First' String
+  -> Bool
+
+main :: IO ()
+main = do
+  quickCheck (monoidAssoc :: FirstMappend)
+  quickCheck (monoidLeftIdentity :: First' String -> Bool)
+  quickCheck (monoidRightIdentity :: First' String -> Bool)
---
+++ Chap15ex.hs
@@ -0,0 +1,236 @@
+module Chap15Ex where
+
+import Data.Semigroup
+import Test.QuickCheck (arbitrary,
+                        coarbitrary,
+                        Arbitrary,
+                        CoArbitrary,
+                        elements,
+                        Gen,
+                        quickCheck,
+                        verboseCheck,
+                        sample,
+                        frequency,
+                        variant)
+
+data Trivial = Trivial deriving (Eq, Show)
+
+instance Semigroup Trivial where
+  _ <> _ = Trivial
+
+instance Monoid Trivial where
+  mempty = Trivial
+  mappend = (<>)
+
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
+
+type TrivialAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+newtype Identity a = Identity a deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Identity a) where
+  (Identity x) <> (Identity y) = Identity (x <> y)
+
+instance (Semigroup a, Monoid a) => Monoid (Identity a) where
+  mempty = Identity mempty
+  mappend = (<>)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = do
+    a <- arbitrary
+    return $ Identity a
+
+type IdentityAssoc = Identity String 
+                   -> Identity String
+                   -> Identity String
+                   -> Bool
+
+data Two a b = Two a b deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (Two a b) <> (Two c d) = Two (a <> c) (b <> d)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return $ Two a b
+
+type TwoAssoc = Two String String
+              -> Two String String
+              -> Two String String
+              -> Bool
+
+newtype BoolConj =
+  BoolConj Bool deriving (Eq, Show)
+
+instance Semigroup BoolConj where
+  (BoolConj x) <> (BoolConj y) = BoolConj (x && y)
+
+instance Arbitrary BoolConj where
+  arbitrary = do
+    a <- elements [True, False]
+    return $ BoolConj a
+
+type BoolConjAssoc = BoolConj
+                   -> BoolConj
+                   -> BoolConj
+                   -> Bool
+
+data Or a b =
+    Fst a
+  | Snd b
+  deriving (Eq, Show)
+
+instance Semigroup (Or a b) where 
+  (Snd x) <> _ = Snd x
+  _ <> (Snd x) = Snd x
+  (Fst x) <> (Fst y) = Fst y
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    frequency [ (1, return (Fst a))
+              , (1, return (Snd b)) ]
+
+type OrAssoc = Or String Int
+             -> Or String Int
+             -> Or String Int
+             -> Bool
+
+newtype Combine a b =
+  Combine { unCombine :: (a -> b) }
+
+instance Semigroup b => Semigroup (Combine a b) where
+  (Combine f) <> (Combine g) = Combine (f <> g)
+
+instance CoArbitrary (Combine a b) where
+  coarbitrary (Combine f) = variant 0
+
+instance (CoArbitrary a, Arbitrary b) => Arbitrary (Combine a b) where
+  arbitrary = do
+    f <- arbitrary
+    return $ Combine f
+
+type CombineAssoc a b = Combine a b -> Combine a b -> Combine a b -> Bool
+
+newtype Comp a =
+  Comp { unComp :: a -> a }
+
+instance Semigroup (Comp a) where
+  (Comp f) <> (Comp g) = Comp (g . f)
+
+data Validation a b =
+  Failure a | Success b
+  deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Validation a b) where
+  (Failure x) <> (Failure y) = Failure (x <> y)
+  (Failure x) <> (Success _) = Failure x
+  (Success _) <> (Failure y) = Failure y
+  (Success x) <> (Success _) = Success x
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Validation a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    frequency [ (1, return (Failure a))
+              , (1, return (Success b)) ]
+
+type ValidationAssoc = Validation String String
+                     -> Validation String String
+                     -> Validation String String
+                     -> Bool
+
+newtype AccumulateRight a b =
+  AccumulateRight (Validation a b)
+  deriving (Eq, Show)
+
+instance Semigroup b => Semigroup (AccumulateRight a b) where
+  AccumulateRight(Success x) <> AccumulateRight(Success y)  = AccumulateRight(Success(x <> y))
+  AccumulateRight(Success x) <> AccumulateRight(Failure y)  = AccumulateRight(Success x)
+  AccumulateRight(Failure x) <> AccumulateRight(Success y)  = AccumulateRight(Success y)
+  AccumulateRight(Failure x) <> AccumulateRight(Failure y)  = AccumulateRight(Failure y)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (AccumulateRight a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    frequency [ (1, return (AccumulateRight a))
+              , (1, return (AccumulateRight b)) ]
+
+type AccumulateRightAssoc = AccumulateRight Int String
+                          -> AccumulateRight Int String
+                          -> AccumulateRight Int String
+                          -> Bool
+
+newtype AccumulateBoth a b =
+  AccumulateBoth (Validation a b)
+  deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (AccumulateBoth a b) where
+  AccumulateBoth(Success x) <> AccumulateBoth(Success y)  = AccumulateBoth(Success(x <> y))
+  AccumulateBoth(Success x) <> AccumulateBoth(Failure y)  = AccumulateBoth(Success x)
+  AccumulateBoth(Failure x) <> AccumulateBoth(Success y)  = AccumulateBoth(Success y)
+  AccumulateBoth(Failure x) <> AccumulateBoth(Failure y)  = AccumulateBoth(Failure (x <> y))
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (AccumulateBoth a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    frequency [ (1, return (AccumulateBoth a))
+              , (1, return (AccumulateBoth b)) ]
+
+type AccumulateBothAssoc = AccumulateBoth String String
+                          -> AccumulateBoth String String
+                          -> AccumulateBoth String String
+                          -> Bool
+
+newtype Mem s a =
+  Mem {
+    runMem :: s -> (a ,s)
+}
+
+fstF :: (s -> (a, s)) -> (s -> a)
+fstF f = \x -> fst $ f x
+
+sndF :: (s -> (a, s)) -> (s -> s)
+sndF f = \x -> snd $ f x
+
+instance (Semigroup a, Monoid a) => Monoid (Mem s a) where
+  mempty = Mem (\s -> (mempty, s))
+  mappend a b = Mem (\s -> ((fstF f <> fstF g) s, ((sndF g) . (sndF f)) s))
+                where f = runMem a
+                      g = runMem b
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity :: (Eq m, Monoid m, Semigroup m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity :: (Eq m, Monoid m, Semigroup m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
+
+f' :: Mem Int String
+f' = Mem $ \s -> ("hi", s + 1)
+
+main :: IO ()
+main = do
+  quickCheck (semigroupAssoc :: TrivialAssoc)
+  quickCheck (monoidLeftIdentity :: Trivial -> Bool)
+  quickCheck (monoidRightIdentity :: Trivial -> Bool)
+  quickCheck (semigroupAssoc :: IdentityAssoc)
+  quickCheck (semigroupAssoc :: TwoAssoc)
+  quickCheck (semigroupAssoc :: BoolConjAssoc)
+  quickCheck (semigroupAssoc :: OrAssoc)
+  quickCheck (semigroupAssoc :: ValidationAssoc)
+  quickCheck (semigroupAssoc :: AccumulateRightAssoc)
+  quickCheck (semigroupAssoc :: AccumulateBothAssoc)
+  print $ runMem (mappend f' mempty) 0
+  print $ runMem (mappend mempty f') 0
+  print $ (runMem mempty 0 :: (String, Int))
+  print $ runMem (mappend f' mempty) 0 == runMem f' 0
+  print $ runMem (mappend mempty f') 0 == runMem f' 0
